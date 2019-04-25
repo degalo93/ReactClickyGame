4486e9b07a184a43c927056dc289d128444726fc
degalo93 <denisgalo10@gmail.com>
2019-04-25 00:14:31 -0700

    Updates

---
+++ precache-manifest.838be81e8aab53532f960608c052fe04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2560b52f802b11c3bf332cb99a6d533",
+    "url": "/.github.io/NewReactapp/index.html"
+  },
+  {
+    "revision": "d015632ceafadf796bdb",
+    "url": "/.github.io/NewReactapp/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/.github.io/NewReactapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d015632ceafadf796bdb",
+    "url": "/.github.io/NewReactapp/static/js/main.5bc742e1.chunk.js"
+  },
+  {
+    "revision": "d8f865377716be98a8c0",
+    "url": "/.github.io/NewReactapp/static/js/runtime~main.98ae4bd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/.github.io/NewReactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
