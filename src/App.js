1ccfc8c38a843b4e9a8789ea197ff5e7d2407d22
degalo93 <denisgalo10@gmail.com>
2019-04-25 20:40:25 -0700

    change file structure

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import Cards from "./components/Cards";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import characters from "./characters.json";
+import Nav from "./components/Nav"
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    characters
+    characters, 
+
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+ 
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Nav />
+        <Title>Match the character</Title>
         {this.state.friends.map(friend => (
           <Cards
             removeFriend={this.removeFriend}
